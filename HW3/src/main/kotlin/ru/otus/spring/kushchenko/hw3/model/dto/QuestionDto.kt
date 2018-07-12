d731c78d7b3cfc88c407cc9c3ce3ade2cd902425
Elena_Kushchenko <Elena_Kushchenko@epam.com>
2018-07-12 03:42:49 +0300

    Revert "hw3 completed"
    This reverts commit 34017a0

--- HW3/src/main/kotlin/ru/otus/spring/kushchenko/hw3/model/dto/QuestionDto.kt
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.kushchenko.hw3.model.dto
-
-data class QuestionDto(
-        val id: Int,
-        val text: String,
-        val answers: List<String>
-)
--- HW3/src/main/kotlin/ru/otus/spring/kushchenko/hw3/util/Util.kt
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.kushchenko.hw3.util
-
-/**
- * Created by Елена on Июль, 2018
- */
-object Util {
-    fun String?.notBlankOrDefault(default: String) =
-            if (this.isNullOrBlank()) default else this
-}
