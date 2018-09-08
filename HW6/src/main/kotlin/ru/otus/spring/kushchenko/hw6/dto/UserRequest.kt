74f590fb43da9fcf2ec2bf25ea880d9934f64be9
Elena_Kushchenko <Elena_Kushchenko@epam.com>
2018-09-08 05:06:51 +0300

    Revert "hw6"
    This reverts commit b4ad1c9

--- HW6/src/main/kotlin/ru/otus/spring/kushchenko/hw6/dto/UserRequest.kt
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.kushchenko.hw6.dto
-
-/**
- * Created by Елена on Авг., 2018
- */
-data class UserRequest(
-    val name: String
-)
--- HW6/src/main/kotlin/ru/otus/spring/kushchenko/hw6/repository/LibraryDao.kt
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.kushchenko.hw6.repository
-
-/**
- * Created by Елена on Июль, 2018
- */
-interface LibraryDao {
-    fun takeBook(bookId: Int, userId: Int)
-    fun returnBook(bookId: Int, userId: Int)
-}
