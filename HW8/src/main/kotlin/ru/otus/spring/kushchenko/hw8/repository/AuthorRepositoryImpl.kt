e76cb9bff77f90bd59a6603a816e94107b65ff6d
Elena_Kushchenko <Elena_Kushchenko@epam.com>
2018-09-18 23:34:08 +0300

    hw8 in process

--- HW8/src/main/kotlin/ru/otus/spring/kushchenko/hw8/repository/AuthorRepositoryImpl.kt
+++ HW8/src/main/kotlin/ru/otus/spring/kushchenko/hw8/repository/AuthorRepositoryImpl.kt
@@ -1,19 +1,21 @@
 package ru.otus.spring.kushchenko.hw8.repository
 
+import com.mongodb.DBObject
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Repository
 import ru.otus.spring.kushchenko.hw8.entity.Author
-import org.springframework.data.mongodb.core.query.BasicQuery
-import org.springframework.data.mongodb.core.query.Criteria
-
 
 @Repository
 class AuthorRepositoryImpl(private val template: MongoTemplate): AuthorRepository {
 
-    override fun getAll(): List<Author> {
-        val query = Query().fields().include("authors").exclude("id").fieldsObject
-
-        return template.find(Query(Criteria.byExample(query)), Author::class.java)
-    }
+    override fun getAll(): List<Author> =
+        template.getCollection("books")
+            .distinct("authors", DBObject::class.java)
+            .toList()
+            .map {
+                Author(
+                    firstName = it.get("firstName") as String,
+                    secondName = it.get("secondName") as String
+                )
+            }
 }
--- HW8/src/main/kotlin/ru/otus/spring/kushchenko/hw8/repository/BookRepository.kt
+++ HW8/src/main/kotlin/ru/otus/spring/kushchenko/hw8/repository/BookRepository.kt
@@ -3,4 +3,4 @@ package ru.otus.spring.kushchenko.hw8.repository
 import org.springframework.data.mongodb.repository.MongoRepository
 import ru.otus.spring.kushchenko.hw8.entity.Book
 
-interface BookRepository : MongoRepository<Book, Int>
+interface BookRepository : MongoRepository<Book, String>
