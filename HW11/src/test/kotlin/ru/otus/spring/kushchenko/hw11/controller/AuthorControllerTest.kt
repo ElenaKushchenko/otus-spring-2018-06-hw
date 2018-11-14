d16a35f3500b25113870941e3611f0fbd51bd5cb
Elena_Kushchenko <Elena_Kushchenko@epam.com>
2018-11-14 05:59:29 +0300

    added tests

--- HW11/src/test/kotlin/ru/otus/spring/kushchenko/hw11/controller/AuthorControllerTest.kt
+++ HW11/src/test/kotlin/ru/otus/spring/kushchenko/hw11/controller/AuthorControllerTest.kt
@@ -1,53 +1,52 @@
-//package ru.otus.spring.kushchenko.hw11.controller
-//
-//import com.nhaarman.mockito_kotlin.verify
-//import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
-//import com.nhaarman.mockito_kotlin.whenever
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.extension.ExtendWith
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-//import org.springframework.boot.test.mock.mockito.MockBean
-//import org.springframework.http.MediaType
-//import org.springframework.test.context.junit.jupiter.SpringExtension
-//import org.springframework.test.web.servlet.MockMvc
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-//import ru.otus.spring.kushchenko.hw11.model.Author
-//import ru.otus.spring.kushchenko.hw11.service.AuthorService
-//import ru.otus.spring.kushchenko.hw11.util.Util.asJsonString
-//import java.time.LocalDate
-//
-///**
-// * Created by Елена on Авг., 2018
-// */
-//@ExtendWith(SpringExtension::class)
-//@WebMvcTest(AuthorController::class)
-//class AuthorControllerTest {
-//    private val BASE_URL = "/authors"
-//
-//    @Autowired
-//    private lateinit var mockMvc: MockMvc
-//    @MockBean
-//    private lateinit var service: AuthorService
-//
-//    @Test
-//    fun getAll() {
-//        val authors = listOf(
-//            Author("Author1", "USA", LocalDate.parse("1901-01-01")),
-//            Author("Author2", "Russia", LocalDate.parse("1902-02-02"))
-//        )
-//
-//        whenever(service.getAll()).thenReturn(authors)
-//
-//        mockMvc.perform(get(BASE_URL))
-//            .andExpect(status().isOk)
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//            .andExpect(content().json(authors.asJsonString()))
-//            .andReturn().response
-//
-//        verify(service).getAll()
-//        verifyNoMoreInteractions(service)
-//    }
-//}
+package ru.otus.spring.kushchenko.hw11.controller
+
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Flux
+import ru.otus.spring.kushchenko.hw11.model.Author
+import ru.otus.spring.kushchenko.hw11.service.AuthorService
+import java.time.LocalDate
+
+/**
+ * Created by Елена on Авг., 2018
+ */
+@ExtendWith(SpringExtension::class)
+@WebFluxTest(AuthorController::class)
+class AuthorControllerTest {
+    private val BASE_URL = "/authors"
+
+    @Autowired
+    private lateinit var webClient: WebTestClient
+    @MockBean
+    private lateinit var service: AuthorService
+
+    @Test
+    fun getAll() {
+        val authors = listOf(
+            Author("Author1", "USA", LocalDate.parse("1901-01-01")),
+            Author("Author2", "Russia", LocalDate.parse("1902-02-02"))
+        )
+
+        whenever(service.getAll()).thenReturn(Flux.fromIterable(authors))
+
+        webClient.get().uri(BASE_URL)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(Author::class.java)
+            .hasSize(authors.size)
+            .contains(*authors.toTypedArray())
+
+        verify(service).getAll()
+        verifyNoMoreInteractions(service)
+    }
+}
--- HW11/src/test/kotlin/ru/otus/spring/kushchenko/hw11/controller/UserControllerTest.kt
+++ HW11/src/test/kotlin/ru/otus/spring/kushchenko/hw11/controller/UserControllerTest.kt
@@ -1,126 +1,127 @@
-//package ru.otus.spring.kushchenko.hw11.controller
-//
-//import com.nhaarman.mockito_kotlin.doNothing
-//import com.nhaarman.mockito_kotlin.verify
-//import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
-//import com.nhaarman.mockito_kotlin.whenever
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.extension.ExtendWith
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-//import org.springframework.boot.test.mock.mockito.MockBean
-//import org.springframework.http.MediaType
-//import org.springframework.test.context.junit.jupiter.SpringExtension
-//import org.springframework.test.web.servlet.MockMvc
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-//import ru.otus.spring.kushchenko.hw11.model.User
-//import ru.otus.spring.kushchenko.hw11.service.UserService
-//import ru.otus.spring.kushchenko.hw11.util.Util.asJsonString
-//
-///**
-// * Created by Елена on Авг., 2018
-// */
-//@ExtendWith(SpringExtension::class)
-//@WebMvcTest(UserController::class)
-//class UserControllerTest {
-//    private val BASE_URL = "/users"
-//
-//    @Autowired
-//    private lateinit var mockMvc: MockMvc
-//    @MockBean
-//    private lateinit var service: UserService
-//
-//    @Test
-//    fun getAll() {
-//        val users = listOf(
-//            User("1", "User1"),
-//            User("2", "User2")
-//        )
-//
-//        whenever(service.getAll()).thenReturn(users)
-//
-//        mockMvc.perform(get(BASE_URL))
-//            .andExpect(status().isOk)
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//            .andExpect(content().json(users.asJsonString()))
-//            .andReturn().response
-//
-//
-//        verify(service).getAll()
-//        verifyNoMoreInteractions(service)
-//    }
-//
-//    @Test
-//    fun get() {
-//        val userId = "1"
-//        val user = User(userId, "User1")
-//
-//        whenever(service.get(userId)).thenReturn(user)
-//
-//        mockMvc.perform(get("$BASE_URL/{id}", userId))
-//            .andExpect(status().isOk)
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//            .andExpect(content().json(user.asJsonString()))
-//            .andReturn().response
-//
-//        verify(service).get(userId)
-//        verifyNoMoreInteractions(service)
-//    }
-//
-//    @Test
-//    fun create() {
-//        val user = User("1", "User1")
-//
-//        whenever(service.create(user)).thenReturn(user)
-//
-//        mockMvc.perform(
-//            post(BASE_URL)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(user.asJsonString())
-//        ).andDo(::print)
-//            .andExpect(status().isOk)
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//            .andExpect(content().json(user.asJsonString()))
-//            .andReturn().response
-//
-//        verify(service).create(user)
-//        verifyNoMoreInteractions(service)
-//    }
-//
-//    @Test
-//    fun update() {
-//        val userId = "1"
-//        val user = User(userId, "User1")
-//
-//        whenever(service.update(user)).thenReturn(user)
-//
-//        mockMvc.perform(
-//            put("$BASE_URL/{id}", userId)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(user.asJsonString())
-//        ).andDo(::print)
-//            .andExpect(status().isOk)
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//            .andExpect(content().json(user.asJsonString()))
-//            .andReturn().response
-//
-//        verify(service).update(user)
-//        verifyNoMoreInteractions(service)
-//    }
-//
-//    @Test
-//    fun delete() {
-//        val userId = "1"
-//
-//        doNothing().whenever(service).delete(userId)
-//        mockMvc.perform(
-//            delete("$BASE_URL/{id}", userId)
-//        )
-//            .andExpect(status().isOk)
-//
-//        verify(service).delete(userId)
-//        verifyNoMoreInteractions(service)
-//    }
-//}
+package ru.otus.spring.kushchenko.hw11.controller
+
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import ru.otus.spring.kushchenko.hw11.model.User
+import ru.otus.spring.kushchenko.hw11.service.UserService
+
+/**
+ * Created by Елена on Авг., 2018
+ */
+@ExtendWith(SpringExtension::class)
+@WebFluxTest(UserController::class)
+class UserControllerTest {
+    private val BASE_URL = "/users"
+
+    @Autowired
+    private lateinit var webClient: WebTestClient
+    @MockBean
+    private lateinit var service: UserService
+
+    @Test
+    fun getAll() {
+        val users = listOf(
+            User("1", "User1"),
+            User("2", "User2")
+        )
+
+        whenever(service.getAll()).thenReturn(Flux.fromIterable(users))
+
+        webClient.get().uri(BASE_URL)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(User::class.java)
+            .hasSize(users.size)
+            .contains(*users.toTypedArray())
+
+        verify(service).getAll()
+        verifyNoMoreInteractions(service)
+    }
+
+    @Test
+    fun get() {
+        val userId = "1"
+        val user = User(userId, "User1")
+
+        whenever(service.get(userId)).thenReturn(Mono.just(user))
+
+        webClient.get().uri("$BASE_URL/{id}", userId)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(User::class.java)
+            .returnResult()
+            .apply { Assertions.assertEquals(user, responseBody) }
+
+        verify(service).get(userId)
+        verifyNoMoreInteractions(service)
+    }
+
+    @Test
+    fun create() {
+        val user = User("1", "User1")
+
+        whenever(service.create(user)).thenReturn(Mono.just(user))
+
+        webClient.post().uri(BASE_URL)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .body(BodyInserters.fromObject(user))
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(User::class.java)
+            .returnResult()
+            .apply { Assertions.assertEquals(user, responseBody) }
+
+        verify(service).create(user)
+        verifyNoMoreInteractions(service)
+    }
+
+    @Test
+    fun update() {
+        val userId = "1"
+        val user = User(userId, "User1")
+
+        whenever(service.update(user)).thenReturn(Mono.just(user))
+
+        webClient.put().uri("$BASE_URL/{id}", userId)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .body(BodyInserters.fromObject(user))
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(User::class.java)
+            .returnResult()
+            .apply { Assertions.assertEquals(user, responseBody) }
+
+        verify(service).update(user)
+        verifyNoMoreInteractions(service)
+    }
+
+    @Test
+    fun delete() {
+        val userId = "1"
+
+        whenever(service.delete(userId)).thenReturn(Mono.empty<Void>())
+
+        webClient.delete().uri("$BASE_URL/{id}", userId)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .exchange()
+            .expectStatus().isOk
+
+        verify(service).delete(userId)
+        verifyNoMoreInteractions(service)
+    }
+}
